1153bcaa06e66551612e3fe94b572c1da5bd5b4d
Matt Gomez <matthewgomezm10@gmail.com>
2026-03-09 12:13:37 -0400

    Refining sensor function and plots for additional sensor handling

--- plot.py
+++ plot.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('sensor_data.csv')
-
+df = pd.read_csv('rangefinder_data.csv')
 
 plt.figure(figsize=(10, 5))
 plt.plot(df['Ray_Index'], df['Distance (m)'], label='Distance (Radial)', marker='o')
@@ -12,4 +11,29 @@ plt.xlabel("Ray Index")
 plt.ylabel("Meters")
 plt.legend()
 plt.grid(True)
+
+df_gyro = pd.read_csv('gyro_data.csv')
+
+plt.figure(figsize=(10, 5))
+plt.plot(df_gyro['Gyro_X'], label='Gyro X', color='r')
+plt.plot(df_gyro['Gyro_Y'], label='Gyro Y', color='g')
+plt.plot(df_gyro['Gyro_Z'], label='Gyro Z', color='b')
+plt.title("Gyroscope Data (Time Series)")
+plt.xlabel("Time Step")
+plt.ylabel("Rad/s")
+plt.legend()
+plt.grid(True)
+
+df_accel = pd.read_csv('accel_data.csv')
+
+plt.figure(figsize=(10, 5))
+plt.plot(df_accel['Accel_X'], label='Accel X', color='r')
+plt.plot(df_accel['Accel_Y'], label='Accel Y', color='g')
+plt.plot(df_accel['Accel_Z'], label='Accel Z', color='b')
+plt.title("Accelerometer Data (Time Series)")
+plt.xlabel("Time Step")
+plt.ylabel("m/s^2")
+plt.legend()
+plt.grid(True)
+
 plt.show()
--- zedm_visualization.py
+++ zedm_visualization.py
@@ -10,34 +10,33 @@ class ZedSim:
         self.model = mujoco.MjModel.from_xml_path(model_path)
         self.data = mujoco.MjData(self.model)
 
-    def get_sensor_data(self):
-        sensor_data =  self.data.sensordata
-        return sensor_data
+    def get_sensor_data(self, sensor_name):
+        sensor_id = self.model.sensor(sensor_name).id
+        sensor_adr = self.model.sensor_adr[sensor_id]
+        sensor_dim = self.model.sensor_dim[sensor_id]
+        return self.data.sensordata[sensor_adr : sensor_adr + sensor_dim].copy()
 
 
-def save_to_csv(raw_data, filename="sensor_data.csv"):
+def save_to_csv(raw_data, filename, columns):
     data_array = np.array(raw_data)
+    num_cols = len(columns)
     
-    try:
-        reshaped_data = data_array.reshape(-1, 8)
-    except ValueError:
-        return None
-
-    columns = [
-        'Distance (m)',
-        'Point_X (m)',
-        'Point_Y (m)',
-        'Point_Z (m)',
-        'Normal_X',
-        'Normal_Y',
-        'Normal_Z',
-        'Depth (planar)'
-    ]
-    
+    reshaped_data = data_array.reshape(-1, num_cols)
     df = pd.DataFrame(reshaped_data, columns=columns)
-    df.insert(0, 'Ray_Index', range(len(df)))
+    
+    if len(df) > 1:
+        df.insert(0, 'Ray_Index', range(len(df)))
+        
     df.to_csv(filename, index=False, mode='w')    
     return df
+
+cam_cols = ['Distance (m)', 
+            'Point_X (m)', 'Point_Y (m)', 'Point_Z (m)', 
+            'Normal_X', 'Normal_Y', 'Normal_Z', 
+            'Depth (planar)']
+gyro_cols = ['Gyro_X', 'Gyro_Y', 'Gyro_Z']
+accel_cols = ['Accel_X', 'Accel_Y', 'Accel_Z']
+
 
 zed = ZedSim("zedm.xml")
 
@@ -47,9 +46,6 @@ with mv.launch_passive(zed.model, zed.data) as viewer:
         viewer.sync()
         
 
-print("Sensor data:", zed.get_sensor_data())
-save_to_csv(zed.get_sensor_data())
-
-
-## evaluate data
-## then control wheels
+save_to_csv(zed.get_sensor_data("zedm_perspective"), "rangefinder_data.csv", cam_cols)
+save_to_csv(zed.get_sensor_data("zedm_gyro"), "gyro_data.csv", gyro_cols)
+save_to_csv(zed.get_sensor_data("zedm_accel"), "accel_data.csv", accel_cols)
